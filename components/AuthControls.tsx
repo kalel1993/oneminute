c9e640fd7daaede6e646926aa37c468d59e8c592
kalel1993 <137779292+kalel1993@users.noreply.github.com>
2026-08-25 10:56:53 +0100

    Fix Clerk Core 3 auth controls

--- components/AuthControls.tsx
+++ components/AuthControls.tsx
@@ -1,4 +1,4 @@
 'use client';
 import Link from 'next/link';
-import {SignedIn,SignedOut,UserButton} from '@clerk/nextjs';
-export function AuthControls(){return <><SignedOut><Link href="/sign-in">Log in</Link></SignedOut><SignedIn><Link href="/account">Account</Link><UserButton/></SignedIn></>}
+import {Show,UserButton} from '@clerk/nextjs';
+export function AuthControls(){return <><Show when="signed-out"><Link href="/sign-in">Log in</Link></Show><Show when="signed-in"><Link href="/account">Account</Link><UserButton/></Show></>}
